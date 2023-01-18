ca53ff4f73b8cc133a5c9890559ff5b38f017271
matsixfive <maffers2006@gmail.com>
2023-01-18 21:13:07 +0000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stop tracking links on YouTube
+
+This [userscript](https://en.wikipedia.org/wiki/Userscript) automatically redirects links in youtube descriptions away from youtube's tracking servers which inhibits them from tracking what links you are visiting as well as speeding up the load times of the websited that are linked to.
+
+## How to install
+
+The most popular userscript manager is [Tampermonkey](https://www.tampermonkey.net/) but others are avaliable. Create a new script and paste in the code from `index.js`.
---
+++ index.js
@@ -0,0 +1,33 @@
+(function () {
+	"use strict";
+
+	const clean = () => {
+		setTimeout(() => {
+			console.log("cleaning");
+			for (let e of document.getElementsByClassName(
+				"yt-simple-endpoint style-scope yt-formatted-string"
+			)) {
+				const searchParams = new URLSearchParams(
+					decodeURIComponent(new URL(e.href).search)
+				);
+				if (searchParams.get("event") === "video_description") {
+					e.href = searchParams.get("q");
+				}
+			}
+		}, 20);
+	};
+
+	document.addEventListener("yt-navigate-finish", clean);
+
+	let int = setInterval(() => {
+		console.log("checking for description");
+		const descText = document.getElementById("description-inline-expander");
+		// const desc = document.getElementById("description");
+		if (descText) {
+			clean();
+			// desc.addEventListener("click", clean)
+			console.log("clearing");
+			// clearInterval(int);
+		}
+	}, 100);
+})();
